a567d28bf2d14f06b3cf91dbd29ffd0e0b8fe66e
David Dunkley <david.dunkley@mopowered.co.uk>
2015-06-18 16:04:18 +0100

    Added Unit and Integration Test Projects.

---
+++ Snippets/MobileWebApiMongoExample/Microsoft/Dunkley.Server/Dunkley.Server.WebApp.IntegrationTests/Repositories/ZipRepositorySpec.cs
@@ -0,0 +1,23 @@
+﻿namespace Dunkley.Server.WebApp.IntegrationTests.Repositories
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Machine.Specifications;
+
+    public class when_creating_a_new_zip
+    {
+        Establish context = () =>
+        {
+        };
+
+        Because of = () => Service.AddItemToQuote("shopId", Quote.Id, Item).Wait();
+
+        It should_add_the_zip_to_the_repository = () => ItemRepository.Verify(x => x.Create("shopId", Quote.Id, Item), Times.Once);
+
+        Cleanup after = () =>
+            Repository.Delete(_shopIdentifier, _quoteId).Await();
+    }
+}
--- Snippets/MobileWebApiMongoExample/Microsoft/Dunkley.Server/Dunkley.Server.WebApp/Repositories/ZipRepository.cs
+++ Snippets/MobileWebApiMongoExample/Microsoft/Dunkley.Server/Dunkley.Server.WebApp/Repositories/ZipRepository.cs
@@ -3,30 +3,40 @@ namespace Dunkley.Server.WebApp.Repositories
     using System.Collections.Generic;
     using System.Threading.Tasks;
     using Models;
+    using MongoDB.Driver;
 
     public class ZipRepository : IZipRepository
     {
-        public Task<Zip> CreateZip(string id, string city, double population, string state, double longitude, double latitude)
+        static IMongoClient _client;
+        static IMongoDatabase _database;
+
+        public ZipRepository(IMongoClient client = null, IMongoDatabase database = null)
+        {
+            _client = client ?? new MongoClient();
+            _database = database ?? _client.GetDatabase("test");
+        }
+
+        public async Task<Zip> CreateZip(string id, string city, double population, string state, double longitude, double latitude)
         {
             throw new System.NotImplementedException();
         }
 
-        public Task<Zip> RetrieveZip(string id)
+        public async Task<Zip> RetrieveZip(string id)
         {
             throw new System.NotImplementedException();
         }
 
-        public Task<IReadOnlyList<Zip>> RetrieveZips()
+        public async Task<IReadOnlyList<Zip>> RetrieveZips()
         {
             throw new System.NotImplementedException();
         }
 
-        public Task<Zip> UpdateZip(string id, string city = null, double? population = null, string state = null, double? longitude = null, double? latitude = null)
+        public async Task<Zip> UpdateZip(string id, string city = null, double? population = null, string state = null, double? longitude = null, double? latitude = null)
         {
             throw new System.NotImplementedException();
         }
 
-        public Task DeleteZip(string id)
+        public async Task DeleteZip(string id)
         {
             throw new System.NotImplementedException();
         }
